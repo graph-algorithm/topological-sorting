6ba2fa63d0c53ab56b240c8921a69e72b2d2f23f
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-09 20:14:39 +0100

    :books: docs(sorted): Fix docs generation.

--- src/sorted.js
+++ src/sorted.js
@@ -7,7 +7,7 @@ import kahn from './kahn.js';
  * Sort the vertices topologically breaking ties according to a given function.
  *
  * @param {Iterable<any>} edges - The input graph as a list of edges.
- * @param {(a: any, b: any) => Number} breakTies - The function to break ties.
+ * @param {Function} breakTies - The function to break ties.
  * @returns {Iterable<any>} The vertices sorted in topological order.
  */
 export default function sorted(edges, breakTies = undefined) {
