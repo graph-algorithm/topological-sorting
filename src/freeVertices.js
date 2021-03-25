8b47fbee584902d9989bd7a6d625a9edb85a4834
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-25 21:02:32 +0100

    :bicyclist: perf(freeVertices): Exploit efficient and idiomatic methods.

--- src/freeVertices.js
+++ src/freeVertices.js
@@ -1,12 +1,12 @@
 /**
  * Compute the free vertices of the input graph.
  *
- * @param {Iterable<any>} graph - The input graph as a list of edges.
- * @returns {Iterable<any>} The free vertices of the input graph.
+ * @param {any} graph - The input graph as a list of edges.
+ * @returns {Iterable} The free vertices of the input graph.
  */
 export default function freeVertices(graph) {
 	const vertices = new Set();
-	for (const [u] of graph) vertices.add(u);
-	for (const [, v] of graph) vertices.delete(v);
+	for (const u of graph.left()) vertices.add(u);
+	for (const v of graph.right()) vertices.delete(v);
 	return vertices;
 }
